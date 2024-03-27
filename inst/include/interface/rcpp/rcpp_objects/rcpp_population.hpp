fc0e99c9c564651f57439894f2b80e29dade4c7a
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2024-03-27 16:16:32 -0700

    add rcpp_population - not working

---
+++ inst/include/interface/rcpp/rcpp_objects/rcpp_population.hpp
@@ -0,0 +1,84 @@
+#ifndef RCPP_POPULATION
+#define RCPP_POPULATION
+
+
+#include "rcpp_interface_base.hpp"
+
+/**
+ * @brief Rcpp interface for Data as an S4 object. To instantiate
+ * from R:
+ *
+ */
+class PopulationInterfaceBase : public RcppInterfaceBase {
+public:
+static uint32_t id_g; /**< static id of the DataInterface object */
+uint32_t id;          /**< local id of the DataInterface object */
+
+/** @brief constructor
+ */
+PopulationInterfaceBase() {
+  this->id = PopulationInterfaceBase::id_g++;
+  RcppInterfaceBase::interface_objects.push_back(this);
+}
+
+/** @brief destructor
+ */
+virtual ~PopulationInterfaceBase() {}
+
+
+/** @brief get_id method for child classes to inherit */
+virtual uint32_t get_id() = 0;
+
+};
+uint32_t PopulationInterfaceBase::id_g = 1;
+
+
+class PopulationInterface  : public PopulationInterfaceBase {
+public:
+    Rcpp::NumericVector ages;
+    
+    PopulationInterface() : PopulationInterfaceBase() {}
+    
+    virtual ~PopulationInterface() {}
+    
+    virtual uint32_t get_id() { return this->id; }
+
+    template<typename Type>
+    bool prepare_local() {
+
+        std::shared_ptr<Model<Type> > model = Model<Type>::getInstance();
+        std::shared_ptr< Population<Type> > pop = 
+            std::make_shared<Population<Type> >();
+        
+        pop->id = this->id;
+        
+        pop->ages.resize(this->ages.size());
+        
+        for(int i =0; i < this->ages.size(); i++){
+            pop->ages[i] = this->ages[i];
+        }
+
+        model->pop = pop;
+     
+        return true;
+    }
+
+     /**
+     * Prepares the model to work with TMB.
+     */
+    virtual bool prepare() {
+
+#ifdef TMB_MODEL
+
+        this->prepare_local<TMB_FIMS_REAL_TYPE>();
+        this->prepare_local<TMB_FIMS_FIRST_ORDER>();
+        this->prepare_local<TMB_FIMS_SECOND_ORDER>();
+        this->prepare_local<TMB_FIMS_THIRD_ORDER>();
+     
+#endif
+   
+        return true;
+    }
+    
+};
+#endif
