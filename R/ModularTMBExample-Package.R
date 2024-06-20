52260a4c6e93d28d3690d051f376e962dba92b28
Andrea-Havron-NOAA <andrea.havron@noaa.gov>
2024-06-19 19:29:50 -0500

    fix documentation

---
+++ R/ModularTMBExample-Package.R
@@ -0,0 +1,18 @@
+## usethis namespace: start
+#' @useDynLib ModularTMBExample, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
+#' @importFrom TMB MakeADFun
+#' @import stats
+#' @import methods
+#' @export CreateModel
+#' @export get_parameter_vector
+#' @export get_data_vector
+#' @export clear
+#' @export Variable
+#' @export VariableVector
+#' @export Population
+#' @export NormalLPDF
+#' @export MVNormLPDF
+#' @export vonBertalanffy
+## usethis namespace: end
+NULL
